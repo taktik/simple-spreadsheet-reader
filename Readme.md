dc1355f3ee26de031cadfb8f00e78571abc27d6b
hubert <hj@taktik.com>
2020-05-05 18:19:17 +0200

    create

---
+++ Readme.md
@@ -0,0 +1,83 @@
+# simple-spreadsheet-reader
+
+A simple reader for a Google spreadsheet publish on web.
+
+## Usage
+
+Install the module from npm with `npm install --save simple-spreadsheet-reader`
+
+```typescript
+import { SpreadsheedReader } from 'simple-spreadsheet-reader'
+
+// Initialize the SpreadsheedReader with the speadsheet Id of URL
+const spreadsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+
+// Load spreadsheed data from google.
+await spreadsheedReader.loadSpreadsheetData()
+
+// get one cell value
+const cellA1 = spreadsheedReader.getCellValue('A1')
+
+const tableElement = spreadsheedReader.getTable()
+
+document.body.append(tableElement)
+```
+Result
+```html
+	 <table class="ssr-table">
+			<thead>
+			<tr>
+				<td class="ssr-cell-head"></td>
+				<td class="ssr-cell-head">A</td>
+				<td class="ssr-cell-head">B</td>
+			</tr>
+			</thead>
+			<tbody>
+			<tr>
+				<td class="ssr-cell-head">1</td>
+				<td cell-id="A1" id="ssr-A1" class="ssr-cell-data">text</td>
+				<td cell-id="B1" id="ssr-B1" class="ssr-cell-data">value</td>
+			</tr>
+			<tr>
+				<td class="ssr-cell-head">2</td>
+				<td cell-id="A2" id="ssr-A2" class="ssr-cell-data"></td>
+				<td cell-id="B2" id="ssr-B2" class="ssr-cell-data">other</td>
+			</tr>
+			</tbody>
+		</table>
+```
+
+### Publishing your Google Sheets to the web
+
+Step 1:
+
+Click File > Publish to the web…
+![](doc-data/publish-spreadsheet.png)
+
+Step 2:
+
+Click Publish, then OK
+![](doc-data/publish.png)
+
+Step 3:
+
+Don't use the link from here.
+![](doc-data/publish-confirmation.png)
+
+
+But copy the URL or the spreadsheet ID form the sheet URL
+![](doc-data/spreadsheet-id.png)
+
+### Common Error
+
+![](doc-data/error-1.png)
+
+If you receive the response below, please go back to Section 2: Publishing your Google Sheets to the web.
+
+![](doc-data/error-2.png)
+
+
+## Api
+
+
+## Development
---
+++ test/test-spreadsheets.ts
@@ -0,0 +1,172 @@
+import { expect } from 'chai'
+import sinon, { SinonFakeServer } from 'sinon'
+import sampleData from './data.json'
+import { template } from 'lodash'
+import { SpreadsheedReader } from '../src/spreadsheedReader'
+import { expectedTable } from './table'
+
+describe('spredsheedReader', () => {
+	let server: SinonFakeServer
+
+	before(() => {
+		server = sinon.fakeServer.create()
+		server.respondWith(
+			'GET',
+			'https://spreadsheets.google.com/feeds/cells/spreadsheets-randomID/1/public/full?alt=json',
+			[
+				200,
+				{ 'Content-Type': 'application/json' },
+				JSON.stringify(sampleData)
+			]
+		)
+		server.respondWith(
+			'GET',
+			'https://spreadsheets.google.com/feeds/cells/invalid-spreadsheet/1/public/full?alt=json',
+			[
+				400,
+				{ 'Content-Type': 'text/html' },
+				JSON.stringify('<body>Error message</body>')
+			]
+		)
+	})
+	describe('constructor', () => {
+		it('should build spreadsheetsIs from on URL', async () => {
+			const spredsheedReader = new SpreadsheedReader('https://docs.google.com/spreadsheets/d/spr1ea_dsh$eets-randomID/edit#gid=0')
+			expect(spredsheedReader.spreadsheetsIs).to.equal('spr1ea_dsh$eets-randomID')
+		})
+		it('should store spreadsheet id', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+			expect(spredsheedReader.spreadsheetsIs).to.equal('spreadsheets-randomID')
+		})
+		it('should not set spreadsheetsIs an error for invalid URL', async () => {
+			const spredsheedReader = new SpreadsheedReader('https://foo.bar')
+			expect(spredsheedReader.spreadsheetsIs).to.be.undefined
+		})
+	})
+	describe('loadRawJson', () => {
+		it('should load valid spreadsheetsJson from spreadsheet id', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			const rawJson = await loadingRawJson
+			expect(rawJson.feed.author[0].name.$t).to.equal('hj')
+			expect(spredsheedReader.xmlError).to.be.undefined
+		})
+		it('should cache valid spreadsheetsJson', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			await loadingRawJson
+			expect(spredsheedReader.rawJson.feed.author[0].name.$t).to.equal('hj')
+			expect(spredsheedReader.xmlError).to.be.undefined
+		})
+		it('should cache valid cellList', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			await loadingRawJson
+			expect(spredsheedReader.cellsList).to.have.length(10)
+			expect(spredsheedReader.cellsList[0]).to.deep.equal({
+				'cell': 'A1',
+				'cellId': 'A1',
+				'coll': 'A',
+				'collNb': 65,
+				'rows': '1',
+				'value': 'text',
+			})
+		})
+		it('should cache maxRaw value', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			await loadingRawJson
+			expect(spredsheedReader.maxRaw).to.equal(4)
+		})
+		it('should cache maxColl value', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			await loadingRawJson
+			expect(spredsheedReader.maxColl).to.equal('E')
+		})
+		it('should fill xmlError for invalid spreadsheets', async () => {
+			const spredsheedReader = new SpreadsheedReader('invalid-spreadsheet')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			try {
+				await loadingRawJson
+				expect(false).to.be.equal(true, 'error not throw loadRawJson')
+			} catch (e) {
+				expect(spredsheedReader.xmlError).to.equal('<body>Error message</body>')
+			}
+		})
+		it('should throw and error for invalid spreadsheets', async () => {
+			const spredsheedReader = new SpreadsheedReader('invalid-spreadsheet')
+			const loadingRawJson = spredsheedReader.loadSpreadsheetData()
+			server.respond()
+			try {
+				await loadingRawJson
+				expect(false).to.be.equal(true, 'error not throw loadRawJson')
+			} catch (e) {
+				expect(e.message).to.equal('Unable to load spreadsheets. For more info see xmlError attribute')
+			}
+		})
+	})
+	describe('getCellValue', () => {
+		it('should return text for a1', () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID');
+			(spredsheedReader as any)._rawJson = sampleData
+			const value = spredsheedReader.getCellValue('a1')
+			expect(value).to.equal('text')
+		})
+		it('should return Hello for B2', () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID');
+			(spredsheedReader as any)._rawJson = sampleData
+			const value = spredsheedReader.getCellValue('B2')
+			expect(value).to.equal('Hello')
+		})
+		it('should return undefined for D1', () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID');
+			(spredsheedReader as any)._rawJson = sampleData
+			const value = spredsheedReader.getCellValue('D1')
+			expect(value).to.be.undefined
+		})
+	})
+	describe('getAllLines', () => {
+		it('should return all the lignes', () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID');
+			(spredsheedReader as any).processSpreadsheets(sampleData)
+			const value = spredsheedReader.getAllLines()
+			expect(value).to.be.deep.equals([
+				['text', 'value', 'autre', undefined, 'enfin'],
+				['welcome', 'Hello', 'Salut', undefined, 'end'],
+				[undefined, undefined, undefined, undefined, undefined],
+				['last line', undefined, undefined, undefined, 'last value'],
+			])
+		})
+	})
+	describe('generate grid', () => {
+		it('lettersGenerator should return letters from A to Z', () => {
+			const letters = Array.from((SpreadsheedReader as any).lettersGenerator('Z'))
+			expect(letters).to.be.deep.equals(Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+		})
+		it('numberGenerator should return Numbers from 1 to 1000', () => {
+			const numbers = Array.from((SpreadsheedReader as any).numberGenerator(1000))
+			expect(numbers).to.have.length(1000)
+			expect(numbers[0]).to.be.equal(1)
+			expect(numbers[999]).to.be.equal(1000)
+		})
+		it('should return table in HTMLElement for valid data', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID');
+			(spredsheedReader as any).processSpreadsheets(sampleData)
+			const xml = spredsheedReader.getTable()
+			expect(xml.innerHTML).to.be.equal(expectedTable)
+		})
+		it('should return HTMLElement of error message for invalid data', async () => {
+			const spredsheedReader = new SpreadsheedReader('spreadsheets-randomID');
+			(spredsheedReader as any)._xmlError = '<div>An error <b>Message</b></div>'
+			const xml = spredsheedReader.getTable()
+			expect(xml.innerHTML).to.be.equal('An error <b>Message</b>')
+		})
+	})
+})
